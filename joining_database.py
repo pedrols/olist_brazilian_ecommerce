89e432a4eb8d96416dd2e48627b9826c9a0061d7
Pedro Lins de Souza <pedrols.oc@gmail.com>
2023-05-03 19:19:25 -0300

    tabelas criadas e consultas simples

--- joining_database.py
+++ joining_database.py
@@ -2,3 +2,46 @@
 
 import pandas as pd
 import numpy as np
+import os
+
+#%% Criando dataframes
+
+fnames = os.listdir('csv')
+print(fnames)
+
+# dicionário com os dataframes
+frames = {}
+for file in fnames:
+    chave = file.replace('.csv','').replace('_dataset','')
+    arquivo = os.path.join('csv', file)
+    frames[chave] = pd.read_csv(arquivo)
+
+#%% Descrever colunas
+
+# %% Criando banco de dados
+from sqlalchemy import create_engine
+# cria uma conexão com um banco de dados SQLite em memória
+# sqlalchemy cria o banco em memória
+engine = create_engine('sqlite://', echo=False)
+
+for chave, df in frames.items():
+    df.to_sql(chave, con=engine)
+
+#%% Testando queries simples
+
+query = """
+SELECT name FROM sqlite_master WHERE type='table';
+"""
+
+df_query = pd.read_sql_query(query, con=engine)
+df_query
+
+# %%
+
+query = '''
+Select * from olist_customers
+limit 10
+'''
+
+df_query = pd.read_sql_query(query, con=engine)
+df_query
